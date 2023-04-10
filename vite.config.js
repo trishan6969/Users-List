92064fb78b0165a9f03d876fd001d2d463b6f698
Trishan Wagle <mailtotrishan@gmail.com>
2023-04-10 11:48:28 +0545

    fix: fixed image issue.

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			src: "/src",
-			components: "/src/components",
 			assets: "/src/assets",
+			components: "/src/components"
 		},
 	},
 });
